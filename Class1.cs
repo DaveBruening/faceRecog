10b2fa7b8f29b8d2d9932cd48c94125a8ebec706
DavidMBruening <76656332+DavidMBruening@users.noreply.github.com>
2021-01-03 02:26:42 -0500

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.Face;
+using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleCoreFaceGitHub {
+    partial class Program {
+        //Since Program.cs was getting so big, I decided to continue class Program in another .cs
+        static string personGroupId = Guid.NewGuid().ToString();
+        static Dictionary<string, string[]> personDictionary;
+        public static async Task<List<DetectedFace>> DetectFaceRecognize(
+            IFaceClient faceClient, string url, string recognition_model) {
+            // Detect faces from image URL. Since only recognizing, use the recognition model 1.
+            // We use detection model 2 because we are not retrieving attributes.
+            IList<DetectedFace> detectedFaces = await faceClient.Face.DetectWithUrlAsync(
+                url, recognitionModel: recognition_model, detectionModel: DetectionModel.Detection02);
+            Console.WriteLine($"{detectedFaces.Count} face(s) detected from image '{Path.GetFileName(url)}'");
+            return detectedFaces.ToList();
+        }
+        public static async Task davesFindSimilar(IFaceClient client, string url, string recognition_model) {
+            Console.WriteLine("========Gip's FIND SIMILAR========");
+            Console.WriteLine();
+            List<string> targetImageFileNames = new List<string> {
+                "Family1-Dad1.jpg",
+                "Family1-Daughter1.jpg",
+                "Family1-Mom1.jpg",
+                "Family1-Son1.jpg",
+                "Family2-Lady1.jpg",
+                "Family2-Man1.jpg",
+                "Family3-Lady1.jpg",
+                "Family3-Man1.jpg"
+            };
+            IList<KeyValuePair<Guid?, string>> ksg = new List<KeyValuePair<Guid?, string>>();
+            //https://csdx.blob.core.windows.net/resources/Face/Images/findsimilar.jpg
+            string sourceImageFileName = "findsimilar.jpg";
+            IList<Guid?> targetFaceIds = new List<Guid?>();
+            foreach (var targetImageFileName in targetImageFileNames) {
+                //Detect faces from target image url
+                var faces = await DetectFaceRecognize(client, $"{url}{targetImageFileName}", recognition_model);
+                //targetFaceIds.Add(faces[0].FaceId.Value);
+                ksg.Add(new KeyValuePair<Guid?, string>(faces[0].FaceId.Value, targetImageFileName));
+            }
+            //Detect faces from source image url:
+            IList<DetectedFace> detectedFaces = await DetectFaceRecognize(client,
+                $"{url}{sourceImageFileName}", recognition_model);
+            Console.WriteLine();
+            ;
+            //Find a similar face(s) in the list of IDs, comparing only the first in list for testing.  
+            IList<SimilarFace> similarResults = await client.Face.FindSimilarAsync(
+                detectedFaces[0].FaceId.Value, null, null, ksg.Select(k => k.Key).ToList());
+            //detectedFaces[0].FaceId.Value, null, null, targetFaceIds);
+            Console.WriteLine($"Faces that are similar to {sourceImageFileName}:");
+            foreach (var similarResult in similarResults)
+                Console.WriteLine($"\tFilename=" +
+                    $"{ksg.Where(g => g.Key == similarResult.FaceId).Select(s => s.Value).First()}" +
+                    $", ID={similarResult.FaceId}, confidence={similarResult.Confidence}.");
+            Console.WriteLine();
+        }
+        public static async Task davesIdentifyInPersonGroup(IFaceClient client, string url, string rcgMdl) {
+            string nl = Environment.NewLine;
+            StringBuilder sbd = new StringBuilder();
+            //Download images from internet: 
+            string imgDir = Directory.GetParent(Directory.GetParent(Directory.GetParent(
+                Environment.CurrentDirectory).FullName).FullName).FullName + @"\identifyImages\";
+            await client.PersonGroup.CreateAsync(personGroupId, personGroupId, recognitionModel: rcgMdl);
+            Console.WriteLine("The PersistedFace GUIDs: ");
+            foreach (string prsn in personDictionary.Keys) {
+                Person person = await client.PersonGroupPerson.CreateAsync(personGroupId: personGroupId, name: prsn);
+                foreach (string pic in personDictionary[prsn]) {
+                    StreamReader srd = new StreamReader(imgDir + pic);
+                    PersistedFace pfc = await client.PersonGroupPerson.AddFaceFromStreamAsync(
+                        personGroupId, person.PersonId, srd.BaseStream, pic);
+                    Console.WriteLine($"{pfc.PersistedFaceId} - {pic} ");
+                }
+            }
+            await client.PersonGroup.TrainAsync(personGroupId);
+            Console.WriteLine("Training Statuses:");
+            while (true) {
+                await Task.Delay(50);
+                var trSt = await client.PersonGroup.GetTrainingStatusAsync(personGroupId);
+                Console.WriteLine("\t" + trSt.Status.ToString());
+                if (trSt.Status == TrainingStatusType.Succeeded)
+                    break;
+            }
+            List<Guid?> guidsPplInFamilyPic = new List<Guid?>();
+            StreamReader strmFullFamily = new StreamReader(imgDir + "identification1.jpg");
+            IList<DetectedFace> idf = await client.Face.DetectWithStreamAsync(strmFullFamily.BaseStream, recognitionModel:
+                RecognitionModel.Recognition03, detectionModel: DetectionModel.Detection02);
+            List<DetectedFace> ldf = idf.ToList();
+            int cntFace = 0;
+            Console.WriteLine($"The {ldf.Count} detected faces in the full family pic: ");
+            foreach (DetectedFace dfc in ldf)
+                guidsPplInFamilyPic.Add(dfc.FaceId);
+            IList<IdentifyResult> iir = await client.Face.IdentifyAsync(guidsPplInFamilyPic, personGroupId);
+            foreach (IdentifyResult ir in iir) {
+                Console.WriteLine($"\tFace{++cntFace}");
+                foreach (IdentifyCandidate icd in ir.Candidates.OrderByDescending(c => c.Confidence)) {
+                    Person prsn = await client.PersonGroupPerson.GetAsync(personGroupId, icd.PersonId);
+                    Console.Write($"\t\t{prsn.Name}: {icd.Confidence} {nl}");
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.Face;
+using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+namespace ConsoleCoreFaceGitHub {
+    partial class Program {
+        const string SUBSCRIPTION_KEY = "3139cbfeb88d552f8fc48a845e1918c2"; //replace with your 32-char key
+        const string ENDPOINT = "https://mycogntvsrv.cognitiveservices.azure.com/"; //replace w/ your cognitive services URL
+        const string IMAGE_BASE_URL = "https://csdx.blob.core.windows.net/resources/Face/Images/";
+        const string imgUrl = //"https://st1.thehealthsite.com/wp-content/uploads/2018/08/Angry-People.jpg";
+            "https://miro.medium.com/max/2560/1*IBNs0gfdHMIgMboGAGK0Uw.jpeg";
+        static List<FaceAttributeType?> lfat;
+        static void Main(string[] args) {
+            const string RECOGNITION_MODEL3 = RecognitionModel.Recognition03;
+            IFaceClient client = Authenticate(ENDPOINT, SUBSCRIPTION_KEY);
+            lfat = Enum.GetValues(typeof(FaceAttributeType)).Cast<FaceAttributeType?>().ToList<FaceAttributeType?>();
+            personDictionary = new Dictionary<string, string[]> {
+                { "Family1-Dad", new[] { "Family1-Dad1.jpg", "Family1-Dad2.jpg" } },
+                  { "Family1-Mom", new[] { "Family1-Mom1.jpg", "Family1-Mom2.jpg" } },
+                  { "Family1-Son", new[] { "Family1-Son1.jpg", "Family1-Son2.jpg" } },
+                  { "Family1-Daughter", new[] { "Family1-Daughter1.jpg", "Family1-Daughter2.jpg" } },
+                  { "Family2-Lady", new[] { "Family2-Lady1.jpg", "Family2-Lady2.jpg" } },
+                  { "Family2-Man", new[] { "Family2-Man1.jpg", "Family2-Man2.jpg" } }
+            };
+            davesImageStream(client, "pic7.jpg", RECOGNITION_MODEL3).Wait();
+            //davesImageUrl(client, imgUrl, RECOGNITION_MODEL3).Wait();
+            //davesFindSimilar(client, IMAGE_BASE_URL, RECOGNITION_MODEL3).Wait();
+            //davesIdentifyInPersonGroup(client, IMAGE_BASE_URL, RECOGNITION_MODEL3).Wait();
+            Console.ReadLine();
+        }
+        public static IFaceClient Authenticate(string endpoint, string key) {
+            return new FaceClient(new ApiKeyServiceClientCredentials(key)) { Endpoint = endpoint };
+        }
+        public static async Task davesImageStream(IFaceClient client, string file, string recogModel) {
+            //Evaluate an image on your computer instead of on a web site:
+            Console.WriteLine("Entering method davesImageStream");
+            //Image class was missing from System.Drawing until I added System.Drawing.Common from nuget
+            //Image img = Image.FromFile("picture1201.jpg");
+            string imgDir = Directory.GetParent(Directory.GetParent(Directory.GetParent(
+                Environment.CurrentDirectory).FullName).FullName).FullName;
+            Stream strm = new FileStream(imgDir + @"\inputImages\" + file, FileMode.Open);
+            IList<DetectedFace> ldf = await client.Face.DetectWithStreamAsync(
+                strm, returnFaceAttributes: lfat, recognitionModel: recogModel
+                , detectionModel: DetectionModel.Detection01);
+            Console.Write($"Face count = {ldf.Count()}");
+            int facCnt = 0;
+            StringBuilder sbd = new StringBuilder();
+            string nl = Environment.NewLine;
+            KeyValuePair<string, double> ksd;
+            Image img = Image.FromFile(imgDir + @"\inputImages\" + file);
+            sbd.Append($"{nl}\timg made from file");
+            Graphics grp = Graphics.FromImage(img);
+            Pen pen = new Pen(Color.Red, (float)3.2);
+            SolidBrush brs = new SolidBrush(Color.Red);
+            foreach (DetectedFace df in ldf) {
+                ksd = df.FaceAttributes.Emotion.ToRankedList().First();
+                sbd.Append($"{nl}Face #{++facCnt}:{nl}" +
+                    $"\tprimary emotion: {ksd.Key}, confidence {ksd.Value}.{nl}"
+                    + $"\tgender:{df.FaceAttributes.Gender}{nl}");
+                if (df.FaceAttributes.Hair.HairColor.Count() > 0) {
+                    HairColor hcl = df.FaceAttributes.Hair.HairColor.OrderByDescending(c => c.Confidence).First();
+                    sbd.Append($"\tprimary Hair Color={hcl.Color}, confidence={hcl.Confidence}{nl}");
+                }
+                else
+                    sbd.Append($"\tHair Color not detected{nl}");
+                sbd.Append($"\tTop={df.FaceRectangle.Top}, Left={df.FaceRectangle.Left}, "
+                    + $"Height={df.FaceRectangle.Height}, Width={df.FaceRectangle.Width}");
+                //if (facCnt == 1) {
+                if (facCnt % 3 == 1) {
+                    pen = new Pen(Color.Red, (float)3.2);
+                    brs = new SolidBrush(Color.Red);
+                }
+                else if (facCnt % 3 == 2) {
+                    pen = new Pen(Color.Green, (float)3.2);
+                    brs = new SolidBrush(Color.Green);
+                }
+                else {
+                    pen = new Pen(Color.Blue, (float)3.2);
+                    brs = new SolidBrush(Color.Blue);
+                }
+                Rectangle rct = new Rectangle(df.FaceRectangle.Left, df.FaceRectangle.Top,
+                    df.FaceRectangle.Width, df.FaceRectangle.Height);
+                grp.DrawRectangle(pen, rct);
+                //sbd.Append($"{nl}\tRectangle drawn on graphic.");
+                Font fnt = new Font("Arial", 12, FontStyle.Regular);
+
+                Point pnt = new Point(df.FaceRectangle.Left, df.FaceRectangle.Top + df.FaceRectangle.Height);
+                grp.DrawString(ksd.Key, fnt, brs, pnt);
+                //sbd.Append($"{nl}\tText drawn on graphic.");
+            }
+            string imgDirFile = imgDir + @"\outputImages\" + file.Substring(0, file.IndexOf(".")) +
+                    "-" + DateTime.Now.Day + "-" + DateTime.Now.Hour.ToString() + "-"
+                    + DateTime.Now.Minute.ToString() + "-" + DateTime.Now.Second.ToString()
+                    + file.Substring(file.IndexOf("."));
+            img.Save(imgDirFile);
+            Console.Write(sbd.ToString());
+        }
+        public static async Task davesImageUrl(IFaceClient client, string url, string recognitionModel) {
+            StringBuilder sbd = new StringBuilder();
+            Console.WriteLine("Running method davesImageUrl, yee-haw!");
+            IList<DetectedFace> ldf = await client.Face.DetectWithUrlAsync(url,
+                returnFaceAttributes: lfat, detectionModel: DetectionModel.Detection01,
+                recognitionModel: recognitionModel);
+            Console.WriteLine("# of faces detected: " + ldf.Count.ToString());
+            sbd.Clear(); int facCnt = 0;
+            foreach (DetectedFace df in ldf) {
+                //foreach (FaceAttribute fa in df.FaceAttributes)  doesn't have GetEnumerator
+                Console.WriteLine($"Face #{++facCnt}:");
+                Console.WriteLine($"\tSmile: {df.FaceAttributes.Smile}, ");
+                KeyValuePair<string, double> ksd = df.FaceAttributes.Emotion.ToRankedList().
+                    OrderByDescending(e => e.Value).First();
+                Console.WriteLine($"\tThe primary emotion is {ksd.Key} with confidence {ksd.Value}");
+                Console.WriteLine("\tThe other emotions are: ");
+                foreach (KeyValuePair<string, double> kvp in df.FaceAttributes.Emotion.
+                    ToRankedList().Where(e => e.Value != ksd.Value))
+                    Console.WriteLine($"\t\t{kvp.Key} = {kvp.Value}");
+                Console.WriteLine("\tGender: " + df.FaceAttributes.Gender.Value.ToString());
+                Console.WriteLine($"\tEye Makup: {df.FaceAttributes.Makeup.EyeMakeup}, " +
+                    $"Lip Makeup: {df.FaceAttributes.Makeup.LipMakeup}");
+                Console.WriteLine($"\tHeadPose: Yaw={df.FaceAttributes.HeadPose.Yaw}, " +
+                    $"Pitch={df.FaceAttributes.HeadPose.Pitch}, Roll={df.FaceAttributes.HeadPose.Roll}");
+                Console.WriteLine($"\tHairColor Count={df.FaceAttributes.Hair.HairColor.Count()}");
+                if (df.FaceAttributes.Hair.HairColor.Count() > 0) {
+                    HairColor hcl = df.FaceAttributes.Hair.HairColor.OrderByDescending(c => c.Confidence).First();
+                    Console.WriteLine($"\tThe primary Hair Color is {hcl.Color} with confidence {hcl.Confidence}");
+                }
+            }
+        }
+    }
+}
